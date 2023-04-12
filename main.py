545ab13df45d75932022a64f55fe8ab388a7d031
George <110062762+georgebarr@users.noreply.github.com>
2023-04-12 21:01:08 +0100

    Added collision detection with wall

--- main.py
+++ main.py
@@ -25,4 +25,8 @@ while game_running:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
 screen.exitonclick()
